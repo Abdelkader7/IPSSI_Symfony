95646f996349737a2e75760d43c722efd7ab9424
Abdelkader <kader.hamra@live.fr>
2019-06-25 10:16:51 +0200

    calculator modif

--- src/Service/Calculator.php
+++ src/Service/Calculator.php
@@ -2,13 +2,17 @@
 
 declare(strict_types=1);
 
+
 namespace App\Service;
+
 
 class Calculator 
 {
-public function add(int $firstNumber, int $secondNumber): int 
-{
-    return ($firstNumber + $secondNumber );
-}
+    public function add(int $firstNumber, int $secondNumber): int 
+    {
+        return $firstNumber + $secondNumber;
+    }
 
 }
+
+?>
