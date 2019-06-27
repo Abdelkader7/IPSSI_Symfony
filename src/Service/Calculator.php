cede0e4228854479385a6abe4a7729f7a7976e90
Abdelkader <kader.hamra@live.fr>
2019-06-27 21:58:55 +0200

    merge

--- src/Service/Calculator.php
+++ src/Service/Calculator.php
@@ -1,12 +1,13 @@
-<?php 
+<?php
 
 declare(strict_types=1);
 
 namespace App\Service;
 
-class Calculator {
-public function add(int $firstNumber, int $secondNumber): int {
-    return ($firstNumber + $secondNumber );
-}
-
+class Calculator
+{
+    public function add(int $firstNumber, int $secondNumber): int
+    {
+        return $firstNumber + $secondNumber;
+    }
 }
